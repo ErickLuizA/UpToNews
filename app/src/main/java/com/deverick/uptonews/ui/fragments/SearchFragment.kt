8cdc85f76dc5990cfd6c16cd8e7f9e9db9a5d8f2
erickLuizA <erickluiz69@gmail.com>
2021-06-12 22:57:38 -0300

    feat: add search layout

--- app/src/main/java/com/deverick/uptonews/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/deverick/uptonews/ui/fragments/SearchFragment.kt
@@ -1,13 +1,15 @@
 package com.deverick.uptonews.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.deverick.uptonews.R
+import android.widget.SearchView
 import com.deverick.uptonews.databinding.FragmentSearchBinding
 
+const val TAG = "SearchFragment"
 
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
@@ -16,9 +18,31 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSearchBinding.inflate(inflater)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.searchView.setOnQueryTextListener(queryTextListener)
+
+        binding.searchView.setOnClickListener {
+            binding.searchView.isIconified = false
+        }
+    }
+
+    private val queryTextListener = object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            Log.d(TAG, query ?: "")
+
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            Log.d(TAG, newText ?: "")
+
+            return true
+        }
     }
 }
