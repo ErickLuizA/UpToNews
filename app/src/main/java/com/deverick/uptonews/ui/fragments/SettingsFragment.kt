f1d3e02c099af65fac0377a7172b27fe83127e54
erickLuizA <erickluiz69@gmail.com>
2021-06-16 15:29:04 -0300

    feat: settings layout

--- app/src/main/java/com/deverick/uptonews/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/deverick/uptonews/ui/fragments/SettingsFragment.kt
@@ -5,19 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.deverick.uptonews.R
 import com.deverick.uptonews.databinding.FragmentSettingsBinding
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSettingsBinding.inflate(inflater)
 
+        auth = FirebaseAuth.getInstance()
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val user = auth.currentUser
+
+        Glide
+            .with(requireContext())
+            .load(user?.photoUrl)
+            .placeholder(R.drawable.ic_baseline_person_24)
+            .error(R.drawable.ic_baseline_person_24)
+            .apply(RequestOptions.circleCropTransform())
+            .into(binding.userAvatar)
+
+        binding.userName.text = user?.displayName
+
+        binding.displayLanguage.setOnClickListener { }
+        binding.displayTheme.setOnClickListener { }
+        binding.favorites.setOnClickListener { }
+        binding.history.setOnClickListener { }
     }
 }
